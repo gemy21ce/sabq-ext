00a26cb01590a86dbd14b6c897707c17bd3fdba3
gemy21ce@gmail.com <gemy21ce@gmail.com@a215b812-09f7-fbc3-3cd3-5dc3666451a5>
2011-11-10 10:17:09 +0000

    updating new ip for sabq server.

--- trunk/controller/data.js
+++ trunk/controller/data.js
@@ -2,29 +2,31 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+//NEW IP :95.211.146.224
 var data={
+    varsion:'1.1.1',
     categories:[{
         id:1,
         title:'\u0633\u0628\u0642 \u0627\u0644\u0645\u062d\u0644\u064a\u0629',//sabq al ma7aleya
-        url:'http://69.65.58.164:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=5',
+        url:'http://95.211.146.224:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=5',
         unreaditems:0,
         active:true
     },{
         id:2,
         title:'\u0633\u0628\u0642 \u0627\u0644\u0631\u064a\u0627\u0636\u064a\u0629',
-        url:'http://69.65.58.164:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=2',
+        url:'http://95.211.146.224:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=2',
         unreaditems:0,
         active:true
     },{
         id:3,
         title:'\u0645\u062d\u0637\u0627\u062a \u0633\u0628\u0642',
-        url:'http://69.65.58.164:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=4',
+        url:'http://95.211.146.224:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=4',
         unreaditems:0,
         active:true
     },{
         id:4,
         title:'\u062a\u062d\u0642\u064a\u0642\u0627\u062a',
-        url:'http://69.65.58.164:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=10',
+        url:'http://95.211.146.224:9000/sabq/api/active?username=active&password=sabq@active1&op=DISPLAY_NEWS_JSON&section=10',
         unreaditems:0,
         active:true
     }]
